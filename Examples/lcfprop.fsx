13f0dc7997628fab87ec16be09742034b7dcd09e
Eric Taucher <Researcher0x00@gmail.com>
2012-09-21 12:03:04 -0400

    Move interactive to script for lcfprop.

--- Examples/lcfprop.fsx
+++ Examples/lcfprop.fsx
@@ -81,7 +81,7 @@
 //open Reasoning.Automated.Harrison.Handbook.dp
 //open Reasoning.Automated.Harrison.Handbook.stal
 //open Reasoning.Automated.Harrison.Handbook.bdd
-//open Reasoning.Automated.Harrison.Handbook.folMod
+open Reasoning.Automated.Harrison.Handbook.folMod
 //open Reasoning.Automated.Harrison.Handbook.skolem
 //open Reasoning.Automated.Harrison.Handbook.herbrand
 //open Reasoning.Automated.Harrison.Handbook.unif
@@ -108,3 +108,28 @@
 //open Reasoning.Automated.Harrison.Handbook.combining
 //open Reasoning.Automated.Harrison.Handbook.lcf
 open Reasoning.Automated.Harrison.Handbook.lcfprop
+        
+// pg. 488
+// ------------------------------------------------------------------------- //
+// The examples in the text.                                                 //
+// ------------------------------------------------------------------------- //
+
+// val it : Reasoning.Automated.Harrison.Handbook.lcf.ProverOperators.thm =
+//   Or
+//     (Imp (Atom (R ("p",[])),Atom (R ("q",[]))),
+//      Imp (Atom (R ("q",[])),Atom (R ("p",[]))))
+lcftaut (parse "(p ==> q) \/ (q ==> p)");;
+
+// val it : Reasoning.Automated.Harrison.Handbook.lcf.ProverOperators.thm =
+//   Iff
+//     (And (Atom (R ("p",[])),Atom (R ("q",[]))),
+//      Iff
+//        (Iff (Atom (R ("p",[])),Atom (R ("q",[]))),
+//         Or (Atom (R ("p",[])),Atom (R ("q",[])))))
+lcftaut (parse "p /\ q <=> ((p <=> q) <=> p \/ q)");;
+
+// val it : Reasoning.Automated.Harrison.Handbook.lcf.ProverOperators.thm =
+//   Iff
+//     (Iff (Iff (Atom (R ("p",[])),Atom (R ("q",[]))),Atom (R ("r",[]))),
+//      Iff (Atom (R ("p",[])),Iff (Atom (R ("q",[])),Atom (R ("r",[])))))
+lcftaut (parse "((p <=> q) <=> r) <=> (p <=> (q <=> r))");;
