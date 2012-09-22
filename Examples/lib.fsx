c18717623988abbd36553a99da413cefa05056ac
Eric Taucher <Researcher0x00@gmail.com>
2012-09-22 15:52:57 -0400

    Verify output of intro script. Change lib license text.
    Signed-off-by: Eric Taucher <Researcher0x00@gmail.com>

--- Examples/lib.fsx
+++ Examples/lib.fsx
@@ -1,62 +1,8 @@
-﻿// IMPORTANT:  READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.
-// By downloading, copying, installing or using the software you agree
-// to this license.  If you do not agree to this license, do not
-// download, install, copy or use the software.
-// 
-// Copyright (c) 2003-2007, John Harrison
-// All rights reserved.
-// 
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 
-// * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-// 
-// * Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the following disclaimer in the
-// documentation and/or other materials provided with the distribution.
-// 
-// * The name of John Harrison may not be used to endorse or promote
-// products derived from this software without specific prior written
-// permission.
-// 
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
-// USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-// OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-// SUCH DAMAGE.
-//
-// ===================================================================
-//
-// Converted to F# 2.0
-//
-// Copyright (c) 2012, Eric Taucher, Jack Pappas
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions
-// are met:
-// 
-// * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the previous disclaimer.
-// 
-// * Redistributions in binary form must reproduce the above copyright
-// notice, this list of conditions and the previous disclaimer in the
-// documentation and/or other materials provided with the distribution.
-// 
-// * The name of Eric Taucher may not be used to endorse or promote
-// products derived from this software without specific prior written
-// permission.
-//
-// ===================================================================
+﻿// ========================================================================= //
+// Copyright (c) 2003-2007, John Harrison.                                   //
+// Copyright (c) 2012 Eric Taucher, Jack Pappas                              //
+// (See "LICENSE.txt" for details.)                                          //
+// ========================================================================= //
 
 #load "initialization.fsx"
 
@@ -64,21 +10,12 @@ open Reasoning.Automated.Harrison.Handbook.lib
 
 // pg. 621
 
-// val smallsqs : func<int,int> =
-//   Branch
-//     (0,1,Leaf (2,[(2, 4)]),
-//      Branch (1,2,Leaf (1,[(1, 1)]),Leaf (3,[(3, 9)])))
 let smallsqs = fpf [1;2;3] [1;4;9];;
 
-// val it : (int * int) list = [(1, 1); (2, 4); (3, 9)]
 graph smallsqs;;
 
-// val it : (int * int) list = [(1, 1); (3, 9)]
 graph (undefine 2 smallsqs);;
 
-// val it : (int * int) list = [(1, 1); (2, 4); (3, 0)]
 graph ((3 |-> 0) smallsqs);;
 
-// val it : int = 9
 apply smallsqs 3;;
-
