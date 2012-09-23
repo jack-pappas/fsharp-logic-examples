12986af8b8037b809604afebd0d1576994b1e52a
Eric Taucher <Researcher0x00@gmail.com>
2012-09-23 19:09:27 -0400

    Verify output of propexamples.fsx.
    Signed-off-by: Eric Taucher <Researcher0x00@gmail.com>

--- Examples/propexamples.fsx
+++ Examples/propexamples.fsx
@@ -17,40 +17,29 @@ open Reasoning.Automated.Harrison.Handbook.propexamples
 // Some currently tractable examples.                                        //
 // ------------------------------------------------------------------------- //
 
-ramsey 3 3 4;;
+print_prop_formula ( ramsey 3 3 4 );;
 
 tautology(ramsey 3 3 5);;
 
 tautology(ramsey 3 3 6);;
-
 
 // pg. 67
 // ------------------------------------------------------------------------- //
 // Example.                                                                  //
 // ------------------------------------------------------------------------- //
 
-// ((OUT_0 <=> (X_0 <=> ~Y_0) <=> ~C_0) /\ (C_1 <=> X_0 /\ Y_0 \/ (X_0 \/ Y_0) /\ C_0)) /\ (OUT_1 <=> (X_1 <=> ~Y_1) <=> ~C_1) /\ (C_2 <=> X_1 /\ Y_1 \/ (X_1 \/ Y_1) /\ C_1)
-// val it : unit = ()
-let funcs = List.map mk_index ["X"; "Y"; "OUT"; "C"];;
-let x = funcs.[0];;
-let y = funcs.[1];;
-let out = funcs.[2];;
-let c = funcs.[3];;
-let f = ripplecarry x y c out 2;;
+let [x; y; out; c] = List.map mk_index ["X"; "Y"; "OUT"; "C"];;
 let pfn = (fun prec p -> print_propvar prec p);;
-print_formula pfn f;;
-    
+print_formula pfn ( ripplecarry x y c out 2 );;
+
 // pg. 72
 // ------------------------------------------------------------------------- //
 // Examples.                                                                 //
 // ------------------------------------------------------------------------- //
 
-// val it : bool = true
 tautology(prime 7);;
 
-// val it : bool = false
 tautology(prime 9);;
 
-// val it : bool = true
 tautology(prime 11);;
 
