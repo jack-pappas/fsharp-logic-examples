f4aff8910daa2e788e6a578dfd6256a95433d922
Phan Anh Dung <phananhdung309@yahoo.com>
2012-10-02 19:09:17 +0200

    Add equivalent tests for bdd.fsx, fol.fsx, herbrand.fsx, tableaux..fsx and unif.fsx
    All 72 tests are passed.
    A few bugs are commented out.

---
+++ Reasoning.Automated.Harrison.Handbook.Tests/bdd.fs
@@ -0,0 +1,44 @@
+﻿// ========================================================================= //
+// Copyright (c) 2003-2007, John Harrison.                                   //
+// Copyright (c) 2012 Eric Taucher, Jack Pappas, Anh-Dung Phan               //
+// (See "LICENSE.txt" for details.)                                          //
+// ========================================================================= //
+
+namespace Reasoning.Automated.Harrison.Handbook.Tests
+
+module bdd =
+    open NUnit.Framework
+    open FsUnit
+
+    open Reasoning.Automated.Harrison.Handbook.lib
+    open Reasoning.Automated.Harrison.Handbook.formulas
+    open Reasoning.Automated.Harrison.Handbook.prop
+    open Reasoning.Automated.Harrison.Handbook.propexamples
+    open Reasoning.Automated.Harrison.Handbook.bdd
+
+    // pg. 105
+    // ------------------------------------------------------------------------- //
+    // Examples.                                                                 //
+    // ------------------------------------------------------------------------- //
+
+    [<Test>]
+    let ``test bddtaut``() =
+        bddtaut (mk_adder_test 4 2)
+        |> should be FsUnit.True
+
+    // pg. 107
+    // ------------------------------------------------------------------------- //
+    // Examples.                                                                 //
+    // ------------------------------------------------------------------------- //
+
+    [<Test>]
+    let ``test ebddtaut with prime``() =
+        ebddtaut (prime 101)
+        |> should be FsUnit.True
+
+    [<Test>]
+    let ``test ebddtaut with mk_adder_test``() =
+        ebddtaut (mk_adder_test 9 5)
+        |> should be FsUnit.True
+
+
