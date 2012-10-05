2cdbd36503a94817e840e4c496811f246ce258a8
Phan Anh Dung <phananhdung309@yahoo.com>
2012-10-05 14:52:45 +0200

    Verify and create test cases for paramodulation.fsx

---
+++ Reasoning.Automated.Harrison.Handbook.Tests/paramodulation.fs
@@ -0,0 +1,20 @@
+﻿// ========================================================================= //
+// Copyright (c) 2003-2007, John Harrison.                                   //
+// Copyright (c) 2012 Eric Taucher, Jack Pappas, Anh-Dung Phan               //
+// (See "LICENSE.txt" for details.)                                          //
+// ========================================================================= //
+
+namespace Reasoning.Automated.Harrison.Handbook.Tests
+
+module paramodulation =
+    open Reasoning.Automated.Harrison.Handbook.folMod
+    open Reasoning.Automated.Harrison.Handbook.paramodulation
+
+    open NUnit.Framework
+    open FsUnit
+
+    [<Test>]
+    let ``test paramodulation``() =
+        paramodulation (parse "(forall x. f(f(x)) = f(x)) /\ (forall x. exists y. f(y) = x)
+                                ==> forall x. f(x) = x")
+       |> should equal [true]
