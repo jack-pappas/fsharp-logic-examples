17583b62e7a71f19bdfa6ec0d6456c0f3b052ff5
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-10-05 11:32:14 -0400

    - Created empty test modules for remaining modules of main project.

---
+++ Reasoning.Automated.Harrison.Handbook.Tests/eqelim.fs
@@ -0,0 +1,15 @@
+﻿// ========================================================================= //
+// Copyright (c) 2003-2007, John Harrison.                                   //
+// Copyright (c) 2012 Eric Taucher, Jack Pappas, Anh-Dung Phan               //
+// (See "LICENSE.txt" for details.)                                          //
+// ========================================================================= //
+
+module Reasoning.Automated.Harrison.Handbook.Tests.eqelim
+
+open Reasoning.Automated.Harrison.Handbook.lib
+open Reasoning.Automated.Harrison.Handbook.eqelim
+open NUnit.Framework
+open FsUnit
+
+// TODO : Implement tests.
+
---
+++ Reasoning.Automated.Harrison.Handbook.Tests/lcffol.fs
@@ -0,0 +1,14 @@
+﻿// ========================================================================= //
+// Copyright (c) 2003-2007, John Harrison.                                   //
+// Copyright (c) 2012 Eric Taucher, Jack Pappas, Anh-Dung Phan               //
+// (See "LICENSE.txt" for details.)                                          //
+// ========================================================================= //
+
+module Reasoning.Automated.Harrison.Handbook.Tests.lcffol
+
+open Reasoning.Automated.Harrison.Handbook.lib
+open Reasoning.Automated.Harrison.Handbook.lcffol
+open NUnit.Framework
+open FsUnit
+
+// TODO : Implement tests.
