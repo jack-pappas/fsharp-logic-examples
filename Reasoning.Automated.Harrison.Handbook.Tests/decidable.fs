bafaf96d8bb2ee14e82aa9894cd7738f3eb8d7b5
Phan Anh Dung <phananhdung309@yahoo.com>
2012-10-05 15:39:44 +0200

    Verify and add tests for decidable.fsx
    156 tests passed
    Time taken: 37 s

---
+++ Reasoning.Automated.Harrison.Handbook.Tests/decidable.fs
@@ -0,0 +1,45 @@
+﻿// ========================================================================= //
+// Copyright (c) 2003-2007, John Harrison.                                   //
+// Copyright (c) 2012 Eric Taucher, Jack Pappas, Anh-Dung Phan               //
+// (See "LICENSE.txt" for details.)                                          //
+// ========================================================================= //
+
+namespace Reasoning.Automated.Harrison.Handbook.Tests
+
+module decidable =
+    open Reasoning.Automated.Harrison.Handbook.lib
+    open Reasoning.Automated.Harrison.Handbook.formulas
+    open Reasoning.Automated.Harrison.Handbook.folMod
+    open Reasoning.Automated.Harrison.Handbook.skolem
+    open Reasoning.Automated.Harrison.Handbook.herbrand
+    open Reasoning.Automated.Harrison.Handbook.tableaux
+    open Reasoning.Automated.Harrison.Handbook.resolution
+    open Reasoning.Automated.Harrison.Handbook.meson
+    open Reasoning.Automated.Harrison.Handbook.decidable
+
+    open NUnit.Framework
+    open FsUnit
+
+    [<Test>]
+    let ``test aedecide``() =
+        aedecide (parse "(forall x y z. P(x,y) /\ P(y,z) ==> P(x,z)) /\ (forall x y z. Q(x,y) /\ Q(y,z) ==> Q(x,z)) /\  (forall x y. P(x,y) ==> P(y,x)) /\ (forall x y. P(x,y) \/ Q(x,y)) ==> (forall x y. P(x,y)) \/ (forall x y. Q(x,y))")
+        |> should be True
+
+    [<Test>]
+    let ``test wang``() =
+        wang (parse "(forall x y. exists z. forall w. P(x) /\ Q(y) ==> R(z) /\ U(w))
+                        ==> (exists x y. P(x) /\ Q(y)) ==> (exists z. R(z))")
+        |> should be True
+
+    [<TestCase("(forall x y. R(x,y) \/ R(y,x)) ==> forall x. R(x,x)", Result=true)>]
+    [<TestCase("(forall x y z. R(x,y) /\ R(y,z) ==> R(x,z)) ==> forall x. R(x,x)", Result=false)>]
+    let ``test decide_fmp`` f =
+        decide_fmp (parse f)
+
+    [<Test>]
+    let ``test decide_monadic``() =
+        decide_monadic (parse "((exists x. forall y. P(x) <=> P(y)) <=>
+                            ((exists x. Q(x)) <=> (forall y. Q(y)))) <=>
+                            ((exists x. forall y. Q(x) <=> Q(y)) <=>
+                           ((exists x. P(x)) <=> (forall y. P(y))))")
+        |> should be True
