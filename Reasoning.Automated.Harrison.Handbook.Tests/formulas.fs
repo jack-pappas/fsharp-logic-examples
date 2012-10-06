e4db8bbd686e4203460db78de7488e96623a53a9
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-10-06 15:22:36 -0400

    - Added a test case for the formula parser.

--- Reasoning.Automated.Harrison.Handbook.Tests/formulas.fs
+++ Reasoning.Automated.Harrison.Handbook.Tests/formulas.fs
@@ -9,10 +9,32 @@ module Reasoning.Automated.Harrison.Handbook.Tests.formulas
 open Reasoning.Automated.Harrison.Handbook.lib
 open Reasoning.Automated.Harrison.Handbook.intro
 open Reasoning.Automated.Harrison.Handbook.formulas
+open Reasoning.Automated.Harrison.Handbook.folMod
 open NUnit.Framework
 open FsUnit
 
 // TODO : Implement unit tests for formulas (particularly parsing and evaluation).
 
+let private parsed_formulas : formula<fol>[] = [|
+    Exists ("a",
+     Exists ("b",
+      And (Atom (R (">", [Var "a"; Fn ("1", [])])),
+       And (Atom (R (">", [Var "b"; Fn ("1", [])])),
+        And
+         (Or (Atom (R ("=", [Fn ("*", [Fn ("2", []); Var "b"]); Var "a"])),
+           Atom
+            (R ("=",
+              [Fn ("*", [Fn ("2", []); Var "b"]);
+               Fn ("+", [Fn ("*", [Fn ("3", []); Var "a"]); Fn ("1", [])])]))),
+         Atom (R ("=", [Var "a"; Var "b"])))))));
+    |]
+
+// This case currently fails (raises exception) when the formula is
+// on two lines; if it's put on a single line, it parses just fine.
+[<TestCase("exists a b. a > 1 /\ b > 1 /\
+               ((2 * b = a) \/ (2 * b = 3 * a + 1)) /\ (a = b)", 0)>]
+let ``parse formulas`` (f, idx) =
+    parse f
+    |> should equal parsed_formulas.[idx]
 
 
