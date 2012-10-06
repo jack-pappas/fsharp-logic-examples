29833d1f0f879f64be14da925a61ac9344f3d0ec
Jack Pappas <jack.pappas+github@tidepowerd.com>
2012-10-06 10:18:01 -0400

    - Created empty test module for formulas, so we can add unit tests for parsing and evaluation.

---
+++ Reasoning.Automated.Harrison.Handbook.Tests/formulas.fs
@@ -0,0 +1,18 @@
+﻿// ========================================================================= //
+// Copyright (c) 2003-2007, John Harrison.                                   //
+// Copyright (c) 2012 Eric Taucher, Jack Pappas, Anh-Dung Phan               //
+// (See "LICENSE.txt" for details.)                                          //
+// ========================================================================= //
+
+module Reasoning.Automated.Harrison.Handbook.Tests.formulas
+
+open Reasoning.Automated.Harrison.Handbook.lib
+open Reasoning.Automated.Harrison.Handbook.intro
+open Reasoning.Automated.Harrison.Handbook.formulas
+open NUnit.Framework
+open FsUnit
+
+// TODO : Implement unit tests for formulas (particularly parsing and evaluation).
+
+
+
