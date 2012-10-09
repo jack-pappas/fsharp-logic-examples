bdda28faf41992313c8635b7ecfbb6e7600196b1
Phan Anh Dung <phananhdung309@yahoo.com>
2012-10-09 20:44:03 +0200

    Merge test cases to use Category property

--- Reasoning.Automated.Harrison.Handbook.Tests/lcffol.fs
+++ Reasoning.Automated.Harrison.Handbook.Tests/lcffol.fs
@@ -195,17 +195,13 @@ let private lcffol_gilmore_results = [|
                                             (forall u. exists v. F(z,u,v) /\ G(y,u) /\ ~H(z,y)))"; // 6
                                     |]
 
+[<TestCase(@"exists x. forall y z. ((F(y,z) ==> (G(y) ==> H(x))) ==> F(x,x)) /\ ((F(z,x) ==> G(x)) ==> H(z)) /\ F(x,y) ==> F(z,z)", 0, Category="LongRunning")>]
+[<TestCase(@"exists x y. forall z. (F(x,y) ==> F(y,z) /\ F(z,z)) /\ (F(x,y) /\ G(x,y) ==> G(x,z) /\ G(z,z))", 1, Category="LongRunning")>]
 [<TestCase(@"(forall x. exists y. F(x,y) \/ F(y,x)) /\ (forall x y. F(y,x) ==> F(y,y)) ==> exists z. F(z,z)", 2)>]
 [<TestCase(@"forall x. exists y. (exists u. forall v. F(u,x) ==> G(v,u) /\ G(u,x)) ==> (exists u. forall v. F(u,y) ==> G(v,u) /\ G(u,y)) \/ (forall u v. exists w. G(v,u) \/ H(w,y,u) ==> G(u,w))", 3)>]
 [<TestCase(@"(forall x. K(x) ==> exists y. L(y) /\ (F(x,y) ==> G(x,y))) /\ (exists z. K(z) /\ forall u. L(u) ==> F(z,u)) ==> exists v w. K(v) /\ L(w) /\ G(v,w)", 4)>]
-let ``lcffol gilmore fast`` (f, idx) =
-    lcffol (parse f) 
-    |> should equal (parse lcffol_gilmore_results.[idx])
-
-[<TestCase(@"exists x. forall y z. ((F(y,z) ==> (G(y) ==> H(x))) ==> F(x,x)) /\ ((F(z,x) ==> G(x)) ==> H(z)) /\ F(x,y) ==> F(z,z)", 0); Category("LongRunning")>]
-[<TestCase(@"exists x y. forall z. (F(x,y) ==> F(y,z) /\ F(z,z)) /\ (F(x,y) /\ G(x,y) ==> G(x,z) /\ G(z,z))", 1); Category("LongRunning")>]
-[<TestCase(@"exists x. forall y z. ((F(y,z) ==> (G(y) ==> (forall u. exists v. H(u,v,x)))) ==> F(x,x)) /\ ((F(z,x) ==> G(x)) ==> (forall u. exists v. H(u,v,z))) /\ F(x,y) ==> F(z,z)", 5); Category("LongRunning")>]
-[<TestCase(@"forall x. exists y. forall z. ((forall u. exists v. F(y,u,v) /\ G(y,u) /\ ~H(y,x)) ==> (forall u. exists v. F(x,u,v) /\ G(z,u) /\ ~H(x,z)) ==> (forall u. exists v. F(x,u,v) /\ G(y,u) /\ ~H(x,y))) /\ ((forall u. exists v. F(x,u,v) /\ G(y,u) /\ ~H(x,y)) ==> ~(forall u. exists v. F(x,u,v) /\ G(z,u) /\ ~H(x,z)) ==> (forall u. exists v. F(y,u,v) /\ G(y,u) /\ ~H(y,x)) /\ (forall u. exists v. F(z,u,v) /\ G(y,u) /\ ~H(z,y)))", 6); Category("LongRunning")>]
-let ``lcffol gilmore slow`` (f, idx) =
+[<TestCase(@"exists x. forall y z. ((F(y,z) ==> (G(y) ==> (forall u. exists v. H(u,v,x)))) ==> F(x,x)) /\ ((F(z,x) ==> G(x)) ==> (forall u. exists v. H(u,v,z))) /\ F(x,y) ==> F(z,z)", 5, Category="LongRunning")>]
+[<TestCase(@"forall x. exists y. forall z. ((forall u. exists v. F(y,u,v) /\ G(y,u) /\ ~H(y,x)) ==> (forall u. exists v. F(x,u,v) /\ G(z,u) /\ ~H(x,z)) ==> (forall u. exists v. F(x,u,v) /\ G(y,u) /\ ~H(x,y))) /\ ((forall u. exists v. F(x,u,v) /\ G(y,u) /\ ~H(x,y)) ==> ~(forall u. exists v. F(x,u,v) /\ G(z,u) /\ ~H(x,z)) ==> (forall u. exists v. F(y,u,v) /\ G(y,u) /\ ~H(y,x)) /\ (forall u. exists v. F(z,u,v) /\ G(y,u) /\ ~H(z,y)))", 6, Category="LongRunning")>]
+let ``lcffol gilmore`` (f, idx) =
     lcffol (parse f) 
     |> should equal (parse lcffol_gilmore_results.[idx])
