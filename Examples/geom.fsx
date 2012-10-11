7aeed8b37c10edc55185ff2ae03736c61a67bfc7
Phan Anh Dung <phananhdung309@yahoo.com>
2012-10-12 01:05:38 +0200

    Removed outdated comments, cleanup functions

--- Examples/geom.fsx
+++ Examples/geom.fsx
@@ -626,16 +626,16 @@ List.partition (grobner_decide << invariant_under_shearing) coordinations;;
 //  One from "Algorithms for Computer Algebra"                                // 
 //  ------------------------------------------------------------------------- // 
 
-grobner_decide << originate) (parse @"is_midpoint(m,a,c) /\ perpendicular(a,c,m,b) ==> lengths_eq(a,b,b,c)";;
+(grobner_decide << originate) (parse @"is_midpoint(m,a,c) /\ perpendicular(a,c,m,b) ==> lengths_eq(a,b,b,c)");;
        
 // pg. 418
 //  ------------------------------------------------------------------------- // 
 //  Parallelogram theorem (Chou's expository example at the start).           // 
 //  ------------------------------------------------------------------------- // 
 
-grobner_decide << originate) (parse @"parallel(a,b,d,c) /\ parallel(a,d,b,c) /\ is_intersection(e,a,c,b,d) ==> lengths_eq(a,e,e,c)";;
+(grobner_decide << originate) (parse @"parallel(a,b,d,c) /\ parallel(a,d,b,c) /\ is_intersection(e,a,c,b,d) ==> lengths_eq(a,e,e,c)");;
 
-grobner_decide << originate) (parse @"parallel(a,b,d,c) /\ parallel(a,d,b,c) /\ is_intersection(e,a,c,b,d) /\ ~collinear(a,b,c) ==> lengths_eq(a,e,e,c)";;
+(grobner_decide << originate) (parse @"parallel(a,b,d,c) /\ parallel(a,d,b,c) /\ is_intersection(e,a,c,b,d) /\ ~collinear(a,b,c) ==> lengths_eq(a,e,e,c)");;
         
 // pg. 421
 //  ------------------------------------------------------------------------- // 
@@ -1280,5 +1280,4 @@ wu butterfly vars003 zeros003;;
 //  Centroid (Chou, example 142).                                             // 
 //  ------------------------------------------------------------------------- // 
 
-// TODO: Fix this: System.DivideByZeroException: Attempted to divide by zero.
 (grobner_decide << originate) (parse @"is_midpoint(d,b,c) /\ is_midpoint(e,a,c) /\ is_midpoint(f,a,b) /\ is_intersection(m,b,e,a,d) ==> collinear(c,f,m)");;
