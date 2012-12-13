a0e134fef80d8a459a0587ea8b877144a2fd6885
Eric Taucher <Researcher0x00@gmail.com>
2012-12-13 16:04:10 -0500

    Add more test and standardize tests for stal.
    Signed-off-by: Eric Taucher <Researcher0x00@gmail.com>

--- FSharpx.Books.AutomatedReasoning.Tests/stal.fs
+++ FSharpx.Books.AutomatedReasoning.Tests/stal.fs
@@ -6,8 +6,6 @@
 
 module FSharpx.Books.AutomatedReasoning.Tests.stal
 
-open NUnit.Framework
-open FsUnit
 open FSharpx.Books.AutomatedReasoning.lib    
 open FSharpx.Books.AutomatedReasoning.formulas
 open FSharpx.Books.AutomatedReasoning.prop
@@ -18,33 +16,57 @@ open FSharpx.Books.AutomatedReasoning.stal
 open FSharp.Compatibility.OCaml
 open Num
 
-// pg. 94
-// ------------------------------------------------------------------------- //
-// An example.                                                               //
-// ------------------------------------------------------------------------- //
+open NUnit.Framework
+open FsUnit
 
-// stal.p001
+let private triggersValues : (string * ((formula<prop> * formula<prop>) * (formula<prop> * formula<prop>) list) list)[] = [| 
+    (
+        // idx 0
+        // stal.p001
+        @"p <=> (q /\ r)",
+        [((Atom (P "p"), formula<prop>.True), [(Atom (P "q"), formula<prop>.True); (Atom (P "r"), formula<prop>.True)]); 
+         ((Atom (P "q"), formula<prop>.True), [(Atom (P "r"), Atom (P "p"))]); 
+         ((Atom (P "q"), Not formula<prop>.True), [(Atom (P "p"), Not formula<prop>.True)]); 
+         ((Atom (P "q"), Not (Atom (P "p"))), [(Atom (P "p"), Not formula<prop>.True); (Atom (P "r"), Atom (P "p"))]); 
+         ((Atom (P "r"), formula<prop>.True), [(Atom (P "q"), Atom (P "p"))]); 
+         ((Atom (P "r"), Atom (P "q")), [(Atom (P "q"), Atom (P "p"))]); 
+         ((Atom (P "r"), Not formula<prop>.True), [(Atom (P "p"), Not formula<prop>.True)]); 
+         ((Atom (P "r"), Not (Atom (P "p"))), [(Atom (P "p"), Not formula<prop>.True); (Atom (P "q"), Atom (P "p"))]); 
+         ((Atom (P "r"), Not (Atom (P "q"))), [(Atom (P "p"), Not formula<prop>.True)])
+        ]
+    );
+    |]
+
+[<TestCase(0, TestName = "stal.p001")>]
+
 [<Test>]
-let ``triggers``() =
-    triggers (parse_prop_formula "p <=> (q /\ r)")
-    |> should equal [
-        ((Atom (P "p"), True), [(Atom (P "q"), True); (Atom (P "r"), True)]); 
-        ((Atom (P "q"), True), [(Atom (P "r"), Atom (P "p"))]); 
-        ((Atom (P "q"), Not True), [(Atom (P "p"), Not True)]); 
-        ((Atom (P "q"), Not (Atom (P "p"))), [(Atom (P "p"), Not True); (Atom (P "r"), Atom (P "p"))]); 
-        ((Atom (P "r"), True), [(Atom (P "q"), Atom (P "p"))]); 
-        ((Atom (P "r"), Atom (P "q")), [(Atom (P "q"), Atom (P "p"))]); 
-        ((Atom (P "r"), Not True), [(Atom (P "p"), Not True)]); 
-        ((Atom (P "r"), Not (Atom (P "p"))), [(Atom (P "p"), Not True); (Atom (P "q"), Atom (P "p"))]); 
-        ((Atom (P "r"), Not (Atom (P "q"))), [(Atom (P "p"), Not True)])]
+let triggers idx = 
+    let (formula, _) = triggersValues.[idx]
+    let (_, result) = triggersValues.[idx]
+    triggers (parse_prop_formula formula)
+    |> should equal result
 
-// pg. 99
-// ------------------------------------------------------------------------- //
-// Examples.                                                                 //
-// ------------------------------------------------------------------------- //
+let private stalmarckValues : (formula<prop> * bool)[] = [| 
+    (
+        // idx 0
+        // stal.p002
+        (mk_adder_test 6 3),
+        true
+    );
+    (
+        // idx 1
+        // stal.p003
+        (mk_adder_test 2 1),
+        true
+    );
+    |]
 
-// stal.p002
+[<TestCase(0, TestName = "stal.p002", Category = "LongRunning")>]
+[<TestCase(1, TestName = "stal.p003")>]
+
 [<Test>]
-let ``stalmarck``() =
-    stalmarck (mk_adder_test 2 1) // use small example (2, 1) instead of (6, 3)
-    |> should be FsUnit.True
+let ``stalmarck tests`` idx = 
+    let (formula, _) = stalmarckValues.[idx]
+    let (_, result) = stalmarckValues.[idx]
+    stalmarck formula
+    |> should equal result
