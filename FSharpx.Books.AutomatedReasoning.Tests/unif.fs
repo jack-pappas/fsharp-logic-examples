b88841acc934a4db3db6e7374e019eb6e8631699
Eric Taucher <Researcher0x00@gmail.com>
2012-12-14 16:02:10 -0500

    Add more test and standardize tests for unif.
    Signed-off-by: Eric Taucher <Researcher0x00@gmail.com>

--- FSharpx.Books.AutomatedReasoning.Tests/unif.fs
+++ FSharpx.Books.AutomatedReasoning.Tests/unif.fs
@@ -13,54 +13,58 @@ open FSharpx.Books.AutomatedReasoning.unif
 open NUnit.Framework
 open FsUnit
 
-// pg. 171
-// ------------------------------------------------------------------------- //
-// Examples.                                                                 //
-// ------------------------------------------------------------------------- //
+let private unify_and_applyValues : ((term * term) list * (term * term) list)[] = [| 
+    (
+        // idx 0
+        // unif.p001
+        [(parset @"f(x,g(y))"),(parset @"f(f(z),w)")],
+        [(Fn ("f",[Fn ("f",[Var "z"]); Fn ("g",[Var "y"])]),
+            Fn ("f",[Fn ("f",[Var "z"]); Fn ("g",[Var "y"])]))]
+    );
+    (
+        // idx 1
+        // unify.p002
+        [(parset @"f(x,y)"),(parset @"f(y,x)")],
+        [(Fn ("f",[Var "y"; Var "y"]), Fn ("f",[Var "y"; Var "y"]))]
+    );
+    (
+        // idx 2
+        // unify.p003
+        [(parset @"f(x,g(y))"),(parset @"f(y,x)")],
+        [(Fn ("f",[Fn ("f",[Var "z"]); Fn ("g",[Var "y"])]),
+            Fn ("f",[Fn ("f",[Var "z"]); Fn ("g",[Var "y"])]))]
+    );
+    (
+        // idx 3
+        // unify.p004
+        [(parset @"x_0"),(parset @"f(x_1,x_1)");
+         (parset @"x_1"),(parset @"f(x_2,x_2)");
+         (parset @"x_2"),(parset @"f(x_3,x_3)")],
+        [(Fn
+            ("f",
+             [Fn
+                ("f",[Fn ("f",[Var "x_3"; Var "x_3"]); Fn ("f",[Var "x_3"; Var "x_3"])]);
+              Fn
+                ("f",[Fn ("f",[Var "x_3"; Var "x_3"]); Fn ("f",[Var "x_3"; Var "x_3"])])]),
+          Fn
+            ("f",
+             [Fn
+                ("f",[Fn ("f",[Var "x_3"; Var "x_3"]); Fn ("f",[Var "x_3"; Var "x_3"])]);
+              Fn
+                ("f",[Fn ("f",[Var "x_3"; Var "x_3"]); Fn ("f",[Var "x_3"; Var "x_3"])])]));
+         (Fn ("f",[Fn ("f",[Var "x_3"; Var "x_3"]); Fn ("f",[Var "x_3"; Var "x_3"])]),
+          Fn ("f",[Fn ("f",[Var "x_3"; Var "x_3"]); Fn ("f",[Var "x_3"; Var "x_3"])]));
+         (Fn ("f",[Var "x_3"; Var "x_3"]), Fn ("f",[Var "x_3"; Var "x_3"]))]
+    );
+    |]
+    
+[<TestCase(0, TestName = "unif.p001")>]
+[<TestCase(1, TestName = "unif.p002")>]
+[<TestCase(2, TestName = "unif.p003", ExpectedException=typeof<System.Exception>, ExpectedMessage="cyclic")>]
+[<TestCase(3, TestName = "unif.p004")>]
 
-// unify.p001
-[<Test>]
-let ``unify_and_apply 1``() =
-    unify_and_apply [(parset "f(x,g(y))"),(parset "f(f(z),w)")]
-    |> should equal [(Fn ("f",[Fn ("f",[Var "z"]); Fn ("g",[Var "y"])]),
-                        Fn ("f",[Fn ("f",[Var "z"]); Fn ("g",[Var "y"])]))]
-
-// unify.p002
-[<Test>]
-let ``unify_and_apply 2``() =
-    unify_and_apply [(parset "f(x,y)"),(parset "f(y,x)")]
-    |> should equal [(Fn ("f",[Var "y"; Var "y"]), Fn ("f",[Var "y"; Var "y"]))]
-
-// unify.p004
-[<Test>]
-let ``unify_and_apply 3``() =
-    unify_and_apply [(parset "x_0"),(parset "f(x_1,x_1)");
-                        (parset "x_1"),(parset "f(x_2,x_2)");
-                        (parset "x_2"),(parset "f(x_3,x_3)")]
-    |> should equal [(Fn
-                          ("f",
-                            [Fn
-                                ("f",
-                                [Fn ("f",[Var "x_3"; Var "x_3"]);
-                                Fn ("f",[Var "x_3"; Var "x_3"])]);
-                            Fn
-                                ("f",
-                                    [Fn ("f",[Var "x_3"; Var "x_3"]);
-                                    Fn ("f",[Var "x_3"; Var "x_3"])])]),
-                        Fn
-                            ("f",
-                            [Fn
-                                ("f",
-                                    [Fn ("f",[Var "x_3"; Var "x_3"]);
-                                    Fn ("f",[Var "x_3"; Var "x_3"])]);
-                            Fn
-                                ("f",
-                                    [Fn ("f",[Var "x_3"; Var "x_3"]);
-                                    Fn ("f",[Var "x_3"; Var "x_3"])])]));
-                        (Fn
-                            ("f",
-                                [Fn ("f",[Var "x_3"; Var "x_3"]); Fn ("f",[Var "x_3"; Var "x_3"])]),
-                         Fn
-                            ("f",
-                                [Fn ("f",[Var "x_3"; Var "x_3"]); Fn ("f",[Var "x_3"; Var "x_3"])]));
-                        (Fn ("f",[Var "x_3"; Var "x_3"]), Fn ("f",[Var "x_3"; Var "x_3"]))]
+let ``unify_and_apply tests`` (idx) =
+    let (eqs, _) = unify_and_applyValues.[idx]
+    let (_, result) = unify_and_applyValues.[idx]
+    unify_and_apply eqs
+    |> should equal result
